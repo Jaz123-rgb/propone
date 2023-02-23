1925903cc2efd2f7f3de3a4b8e5c21d510e85c14
Jaz <pjaziel723@gmail.com>
2023-02-22 20:21:46 -0600

    ahora si

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -10,7 +10,7 @@ const AboutSection = () => {
 						<div>
 								
 								<div className="flex w-full flex-col space-y-10  justify-center align-top md:flex-row">
-								<iframe className='w-full h-64' width="560" height="315" src="https://www.youtube.com/embed/Su_7ddIeW34" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+								<iframe className='w-full h-64'  src="https://www.youtube.com/embed/Su_7ddIeW34" title="YouTube video player" />
 								</div>
 						</div>
 						<div></div>
